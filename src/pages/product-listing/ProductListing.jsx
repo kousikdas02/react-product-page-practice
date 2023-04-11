19e90b19ca1787fb2ed5ff8b39fe043c992dfe6f
kousikdas <kousik.das@webskitters.com>
2023-04-11 19:54:55 +0530

    products office file

--- src/pages/product-listing/ProductListing.jsx
+++ src/pages/product-listing/ProductListing.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ProductListing = () => {
-  return (
-    <>
-        <h1>Product Listing Page</h1>
-    </>
-  )
-}
+    const BaseUrl = "https://dummyjson.com";
+    const [products, setProducts] = useState([]);
 
-export default ProductListing
+    const getAllProducts = async() => {
+        try {
+            const response = await axios.get(`${BaseUrl}/products`)
+            setProducts(response?.data)
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+
+    useEffect(() => {
+        getAllProducts()
+    }, [])
+
+    console.log(products);
+    
+    return (
+        <>
+            <Container>
+                <div className="common_wrappper pt-3 pb-3">
+                    <h1>Product Listing Page</h1>
+
+                    <Row>
+                        {
+                            products.map((productsData) => {
+                                return(
+                                    <>
+                                        <Col lg={4} className="mb-2">
+                                            <div className="product_card">
+                                                <h3 className="product_title">Product Name: {productsData?.title}</h3>
+                                            </div>
+                                        </Col>
+                                    </>
+                                )
+                            })
+                        }
+                    </Row>
+                </div>
+            </Container>
+
+        </>
+    );
+};
+
+export default ProductListing;
